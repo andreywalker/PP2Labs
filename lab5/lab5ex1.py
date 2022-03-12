8f4c944fc369a278a14b83fb2f52497448c4de30
andreywalker <andreyetoja@gmail.com>
2022-03-12 15:31:01 +0600

    8 and 10 tasks

--- lab5/lab5ex1.py
+++ lab5/lab5ex1.py
@@ -91,3 +91,44 @@ s="The capital of great britain is london. Not friggin milan. It is in spain. Le
 ss=re.split("[A-Z]", s)
 
 print(ss)
+
+
+#9
+
+print("\n")
+print("9 task")
+print("\n")
+
+s="The capital of great britain is london. Not friggin milan. It is in spain. Let's make some snake_case here. What a hell?! Who even uses it?! Nobody! Men prefer CamelCase. (Don't even mention kebab-case. Don't. It will cause only EMOTIONAL DAMAGE). Really?! I don't have any word ending with 'b'? it is a FAILURE! Tere are so maby such words: scrab cab cub tub lab pub tab "
+i=0
+def separateCamel(match):
+    global i
+    g=match.group()
+    c=" "
+    g=str(c)+g
+    i+=1
+    return g
+s=re.sub("[A-Z]{1}[a-z]+", separateCamel ,s)
+
+print(s)
+
+
+#10
+
+print("\n")
+print("10 task")
+print("\n")
+
+s="The capital of great britain is london. Not friggin milan. It is in spain. Let's make some snake_case here. What a hell?! Who even uses it?! Nobody! Men prefer CamelCase. (Don't even mention kebab-case. Don't. It will cause only EMOTIONAL DAMAGE). Really?! I don't have any word ending with 'b'? it is a FAILURE! Tere are so maby such words: scrab cab cub tub lab pub tab "
+i=0
+def to_snake(match):
+    global i
+    g=match.group()
+    c=g[len(g)-1]
+    g=g[:len(g)-1]+"_"+str(c).lower()
+    i+=1
+    print(g)
+    return g
+s=re.sub("[a-z]+[A-Z]{1}", to_snake ,s)
+
+print(s)
