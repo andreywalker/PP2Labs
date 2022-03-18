f691e28c54e633b4ffc68b685e35e61c9d8b5436
andreywalker <andreyetoja@gmail.com>
2022-03-19 00:14:35 +0600

    lab6

--- lab6/lab6ex13.py
+++ lab6/lab6ex13.py
@@ -1,3 +1,4 @@
 import os
-if os.access("C:\Users\User\Desktop\PP2Labs\lab6\llll.txt", os.EX_OK) and os.path.exists("C:\Users\User\Desktop\PP2Labs\lab6\llll.txt"):
-    os.remove("C:\Users\User\Desktop\PP2Labs\lab6\llll.txt")
+p=input()
+if os.path.exists(p):
+    os.remove(p)
