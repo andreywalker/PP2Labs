3cf3cf93810170ec9058f0f1bda1bd4aa3201bd6
andreywalker <andreyetoja@gmail.com>
2022-04-14 00:42:03 +0600

    almost working snake

--- lab8/Snake.py
+++ lab8/Snake.py
@@ -1,3 +1,4 @@
+from random import randrange
 import pygame
 pygame.init()
 screen=pygame.display.set_mode((800,800))
@@ -26,12 +27,7 @@ snake=[]
 snake.append((x0,y0))
 
 snake.append((x0-im,y0))
-snake.append((x0-2*im,y0))
-snake.append((x0-3*im,y0))
-snake.append((x0-4*im,y0))
-snake.append((x0-5*im,y0))
-snake.append((x0-6*im,y0))
-snake.append((x0-7*im,y0))
+
 
 #some walls
 walls=[]
@@ -39,16 +35,38 @@ walls.append(pygame.Rect(300,100,im,im))
 for i in range(1,9):
     walls.append(walls[i-1].move(im,0))
 
+apples=[]
+
+
+def genApple():
+    x=randrange(0,800,im)
+    y=randrange(0,800,im)
+    for i in range(0,len(walls)-1):
+        if walls[i].collidepoint(x,y):
+            x=genApple()[0]
+            y=genApple()[1]
+    for i in range(0, len(snake)-1):
+        if snake[i][0]==x and snake[i][1]==y:
+            x=genApple()
+            y=genApple()
+    return (x,y)
+
 #some magic with framerate
 fr=10
 c = pygame.time.Clock()
+its=0
 
 while running:
+    #some another magic this code to work
     c.tick(fr)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
+    #snake ate apple variables
+    ate=False
+    eatenApple=()
+
     #cleaning the screen
     screen.fill((0,0,0))
 
@@ -67,6 +85,23 @@ while running:
         elif di==3 and walls[i].collidepoint(x0,y0+im):
             isDead=True
 
+
+    #checking for eating apples
+    for i in range(0, len(apples)-1):
+        if di ==0 and x0+im==apples[i][0] and y0==apples[i][1]:
+            ate=True
+            eatenApple=i
+        elif di ==2 and x0-im==apples[i][0] and y0==apples[i][1]:
+            ate=True
+            eatenApple=i
+        elif di ==1 and x0==apples[i][0] and y0-im==apples[i][1]:
+            ate=True
+            eatenApple=i
+        elif di ==3 and x0==apples[i][0]  and y0+im==apples[i][1]:
+            ate=True
+            eatenApple=i
+
+
     #checking for collision with tail
     for i in range(0,len(snake)-1):
         if di ==0 and x0+im==snake[i][0] and y0==snake[i][1]:
@@ -77,6 +112,7 @@ while running:
             isDead=True
         elif di ==3 and x0==snake[i][0]  and y0+im==snake[i][1]:
             isDead=True
+
 
     if not isDead:
         #moving right
@@ -101,10 +137,17 @@ while running:
             y0=0
 
         #moving tail of snake
-        for i in range(len(snake)-1,1,-1):
-            snake[i]=snake[i-1]
-        snake[1]=snake[0]
-        print(snake) 
+        if not ate:
+            for i in range(len(snake)-1,1,-1):
+                snake[i]=snake[i-1]
+            snake[1]=snake[0]
+            #print(snake) 
+        else:
+            end=snake[len(snake)-1]
+            for i in range(len(snake)-1,1,-1):
+                snake[i]=snake[i-1]
+            snake[1]=snake[0]
+            snake.append(end)
 
         snake[0]=(x0,y0)
 
@@ -119,7 +162,13 @@ while running:
             di=2
     else:
         screen.blit(text2,(400,400))
+
+    #making apple disappear after eating
+    if ate:
+        apples.pop(eatenApple)
     
+
+    #drawing walls
     for i in range(0,len(walls)-1):
         pygame.draw.rect(screen,(170,170,80),walls[i])
 
@@ -131,4 +180,18 @@ while running:
     for i in range(1, len(snake)):
         pygame.draw.circle(screen,(60,180,160),snake[i],im/2)
 
+    print(str(ate)+"         "+str(eatenApple)+"      "+str(apples))
+
+    #drawing apples
+    for i in range(0,len(apples)-1):
+        pygame.draw.circle(screen,(250,0,0),apples[i],im/2)
+
     pygame.display.update()
+    if not isDead:
+        if its<= 20:
+            its=its+1
+        else:
+            its=0
+            apples.append(genApple())
+    
+        
