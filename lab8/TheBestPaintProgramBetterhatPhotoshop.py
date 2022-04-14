3e6a2beebde10cd213c1917f7dd53d4ddd3f7f1a
andreywalker <andreyetoja@gmail.com>
2022-04-15 00:58:38 +0600

    some paint

---
+++ lab8/TheBestPaintProgramBetterhatPhotoshop.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800,800))
+running=True
+
+screen.fill((0,0,0))
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    if not pygame.mouse.get_pressed()==(0,0,0):
+        pygame.draw.circle(screen,(20,88,99),pygame.mouse.get_pos(),10)
+        if 
+    pygame.display.update()
+    
+    
